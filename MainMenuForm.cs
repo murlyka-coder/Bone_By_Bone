b59e60ed3b80cc94fee46528637dfe8516a6c96a
Kaydzaku <kaydzaku@gmail.com>
2026-06-17 23:12:06 +1000

    создал панели каждой страницы

---
+++ MainMenuForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bone_By_Bone
+{
+    public partial class MainMenuForm : UserControl
+    {
+        public MainMenuForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
