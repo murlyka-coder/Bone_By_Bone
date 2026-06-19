e1a6201007af3bbd1938126ddc5c7e38d940ee8a
Kaydzaku <kaydzaku@gmail.com>
2026-06-19 14:01:32 +1000

    кнопка

--- MainMenuForm.Designer.cs
+++ MainMenuForm.Designer.cs
@@ -28,30 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.btnSettings = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.button1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.BackgroundImage = global::Bone_By_Bone.Properties.Resources.buttonnormal1;
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button1.Location = new System.Drawing.Point(741, 468);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(406, 157);
-            this.button1.TabIndex = 3;
-            this.button1.Text = " Начать игру";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
-            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
-            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
             // 
             // btnSettings
             // 
@@ -64,25 +44,41 @@
             this.btnSettings.UseVisualStyleBackColor = true;
             this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Image = global::Bone_By_Bone.Properties.Resources.buttonnormal1;
+            this.button1.Location = new System.Drawing.Point(672, 466);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(374, 159);
+            this.button1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.button1.TabIndex = 11;
+            this.button1.TabStop = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
             // MainMenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Bone_By_Bone.Properties.Resources.background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.Controls.Add(this.btnSettings);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSettings);
             this.DoubleBuffered = true;
             this.Name = "MainMenuForm";
             this.Size = new System.Drawing.Size(1920, 1080);
             this.Load += new System.EventHandler(this.MainMenuForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.button1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.PictureBox button1;
     }
 }
--- MainMenuForm.cs
+++ MainMenuForm.cs
@@ -40,22 +40,25 @@ namespace Bone_By_Bone
 
         private void button1_MouseEnter(object sender, EventArgs e)
         {
-            button1.BackgroundImage = Properties.Resources.buttonlight1;
-            button1.BackColor = Color.Transparent;
+            button1.Image = Properties.Resources.buttonlight1;
+            button1.Parent.Invalidate(button1.Bounds);
+
         }
 
         private void button1_MouseLeave(object sender, EventArgs e)
         {
-            button1.BackgroundImage = Properties.Resources.buttonnormal1;
-            button1.BackColor = Color.Transparent;
+            button1.Image = Properties.Resources.buttonnormal1;
+            button1.Parent.Invalidate(button1.Bounds);
+
         }
+
 
         private void button1_MouseDown(object sender, MouseEventArgs e)
         {
-            button1.BackgroundImage = Properties.Resources.buttonpress1;
-            button1.BackColor = Color.Transparent;
+            button1.Image = Properties.Resources.buttonpress1;
+            button1.Parent.Invalidate(button1.Bounds);
+
+
         }
-
-
     }
 }
