c4e96191eec5578b36cc219e0e025787687f2d3c
Kaydzaku <kaydzaku@gmail.com>
2026-06-19 12:10:33 +1000

    .

--- MainMenuForm.Designer.cs
+++ MainMenuForm.Designer.cs
@@ -29,14 +29,13 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnSettings = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.button1.Location = new System.Drawing.Point(182, 142);
+            this.button1.Location = new System.Drawing.Point(929, 619);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(200, 53);
@@ -44,17 +43,6 @@
             this.button1.Text = " Начать игру";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F);
-            this.label1.Location = new System.Drawing.Point(162, 85);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(255, 44);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Bone by Bone";
             // 
             // btnSettings
             // 
@@ -71,23 +59,18 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::Bone_By_Bone.Properties.Resources.background;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Controls.Add(this.btnSettings);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Name = "MainMenuForm";
             this.Size = new System.Drawing.Size(1920, 1080);
             this.Load += new System.EventHandler(this.MainMenuForm_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnSettings;
     }
 }
