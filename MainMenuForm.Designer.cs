a33454b821190dfa20cd1a48de78ae21f78fafe1
Kaydzaku <kaydzaku@gmail.com>
2026-06-21 11:34:34 +1000

    кнопка уровня

--- MainMenuForm.Designer.cs
+++ MainMenuForm.Designer.cs
@@ -47,9 +47,9 @@ namespace Bone_By_Bone
             this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Image = global::Bone_By_Bone.Properties.Resources.buttonnormal1;
-            this.button1.Location = new System.Drawing.Point(746, 494);
+            this.button1.Location = new System.Drawing.Point(762, 481);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(435, 137);
+            this.button1.Size = new System.Drawing.Size(408, 142);
             this.button1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.button1.TabIndex = 11;
             this.button1.TabStop = false;
@@ -75,9 +75,9 @@ namespace Bone_By_Bone
             this.button2.BackColor = System.Drawing.Color.Transparent;
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.Image = global::Bone_By_Bone.Properties.Resources.buttonnormal2;
-            this.button2.Location = new System.Drawing.Point(784, 661);
+            this.button2.Location = new System.Drawing.Point(800, 644);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(362, 115);
+            this.button2.Size = new System.Drawing.Size(335, 115);
             this.button2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.button2.TabIndex = 13;
             this.button2.TabStop = false;
@@ -91,9 +91,9 @@ namespace Bone_By_Bone
             this.button3.BackColor = System.Drawing.Color.Transparent;
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button3.Image = global::Bone_By_Bone.Properties.Resources.buttonnormal3;
-            this.button3.Location = new System.Drawing.Point(826, 811);
+            this.button3.Location = new System.Drawing.Point(841, 780);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(278, 93);
+            this.button3.Size = new System.Drawing.Size(251, 90);
             this.button3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.button3.TabIndex = 14;
             this.button3.TabStop = false;
